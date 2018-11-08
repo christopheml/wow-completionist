42251cce6198b3afbb9e26704145307af2131429
Christophe Michel <christopheml@users.noreply.github.com>
2018-11-08 20:37:40 +0100

    Removed superfluous @EnableWebSecurity annotation

--- src/main/java/com/github/christopheml/wowcompletionist/configuration/SecurityConfiguration.java
+++ src/main/java/com/github/christopheml/wowcompletionist/configuration/SecurityConfiguration.java
@@ -2,11 +2,9 @@ package com.github.christopheml.wowcompletionist.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
