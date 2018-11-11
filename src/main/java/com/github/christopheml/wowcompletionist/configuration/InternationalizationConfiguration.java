fceb7421d5a6b5c8acd72339d303e67bae0bf8c7
Christophe Michel <christopheml@users.noreply.github.com>
2018-11-11 01:38:47 +0100

    Added internationalization.
    Text strings are not in a proper resource bundle.

---
+++ src/main/java/com/github/christopheml/wowcompletionist/configuration/InternationalizationConfiguration.java
@@ -0,0 +1,18 @@
+package com.github.christopheml.wowcompletionist.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class InternationalizationConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        return messageSource;
+    }
+
+}
